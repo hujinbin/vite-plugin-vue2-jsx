0cf21f4ef7e29ef658fdc60427f1019e8d160be0
hujinbin <437044048@qq.com>
2022-02-12 20:20:39 +0800

    test: 测试用例

---
+++ test/test.spec.ts
@@ -0,0 +1,41 @@
+import {
+  preTest,
+  postTest,
+  declareTests,
+  startServer,
+  killServer,
+} from './util'
+
+jest.setTimeout(100000)
+
+beforeAll(async () => {
+  await preTest()
+})
+
+afterAll(postTest)
+
+describe('vite-plugin-vue2-jsx', () => {
+  describe('dev', () => {
+    beforeAll(async () => {
+      await startServer(false)
+    })
+
+    afterAll(async () => {
+      await killServer()
+    })
+
+    declareTests(false)
+  })
+
+  describe('build', () => {
+    beforeAll(async () => {
+      await startServer(true)
+    })
+
+    afterAll(async () => {
+      await killServer()
+    })
+
+    declareTests(true)
+  })
+})
